ffc00413e7b0466560ab514bbd0fba628321f256
unknown <ZaharBoxing>
2018-05-20 14:30:19 +0300

    Bot

--- app.py
+++ app.py
@@ -39,8 +39,8 @@ def webhook():
 						messaging_text = 'no text'
 						response = get_no_text()
 						bot.send_text_message(sender_id, response)
-						
-				bot.send_buttons(recipient_id, "你可以透過下列方式找到我", [
+
+				bot.send_buttons(sender_id, "你可以透過下列方式找到我", [
     				ActionButton(ButtonType.WEB_URL, "Blog", "http://blog.enginebai.com"),
 				])
 
