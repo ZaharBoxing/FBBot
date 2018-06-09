0defaeb6233c7633e8b037c08156bd6ede06c567
unknown <ZaharBoxing>
2018-06-09 22:13:07 +0300

    'bot'

--- app.py
+++ app.py
@@ -46,7 +46,7 @@ def webhook():
 					elif entity == 'wit_mood':
 						response = "I am too, do u want some info about company?"
 					elif entity == 'wit_consent':
-						response = bot.send_button_message(recipient_id, buttons)
+						response = bot.send_button_message(recipient_id, text, buttons)
 					elif entity == 'wit_negation':
 						response = "Okey, see u next time. Good bye!"
 
@@ -61,12 +61,13 @@ def log(message):
 	print(message)
 	sys.stdout.flush()
 
-
 buttons = []
 
 button = Button(title='Site', type='web_url', url='http://isport.ua/')
 
 buttons.append(button)
+
+text = "Click here"
 
 
 if __name__ == "__main__":
